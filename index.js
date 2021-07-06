0d1e400182810f9e3506a9ba308ac318ff0ea962
Maruf Hossain <hossain815265@gmail.com>
2021-07-06 17:14:27 +0600

    done

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectID
+require('dotenv').config();
+
+const app = express()
+app.use(express.static('categories'))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(fileUpload({
+    createParentPath: true
+  }));
+
+
+app.get('/', (req, res) => {
+    res.send('everything is ok here')
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.zcphb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useUnifiedTopology: true })
+
+
+
+client.connect(error => {
+    const categoryCollection = client.db("bandhon_ecommerce").collection("categories")
+
+    app.get('/get-categories', (req, res) => {
+        categoryCollection.find({})
+        .toArray((err, docs) => {
+            res.send(docs)
+        })
+    })
+
+    app.patch('/add-brand/id',(req, res)=>{
+        const id = req.query.id;
+        const body = req.body;
+        categoryCollection.updateOne(
+            { _id: ObjectId(id) },
+            {
+              $set: {brands: body.brands},
+            }
+        )
+        .then(result =>  res.send(result))
+    })
+
+    app.patch('/add-product/id',(req, res)=>{
+        const id = req.query.id;
+        const body = req.body;
+        console.log(body)
+        categoryCollection.updateOne(
+            { _id: ObjectId(id) },
+            {
+            $set: {products: body.products},
+            }
+        )
+        .then(result =>  res.send(result))
+    })
+})
+
+app.listen(process.env.PORT || 5000)
