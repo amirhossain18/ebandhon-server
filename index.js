b98178af1a6cda73b169514b17a55f34edad1568
Maruf Hossain <hossain815265@gmail.com>
2021-07-31 21:33:42 +0600

    done

--- index.js
+++ index.js
@@ -72,9 +72,7 @@ client.connect(error => {
         .then(result =>  res.send(result))
         .catch(err => console.log(err))
     })
-})
 
-client.connect(error => {
     const userDataCollection = client.db("bandhon_ecommerce").collection("user_data")
 
     app.get('/get-user-data', (req, res) => {
@@ -108,36 +106,6 @@ client.connect(error => {
         .catch(err => res.send(err))
     })
 
-    // calling sManager payment gateway api
-    app.post('/call-payment-gateway', (req, res) => {
-        const client_id = '664520543'
-        const client_secret = 'E2BC3ADUArXkyjpJJe6nWcBhDbLwBbY5EVQ1yfdwAXPafUnAZcysJm0zQ98Y4TQriiKcZQvQgRsZxlAaDeLZCK5msWzwLRWB4aPHNN1ZTH4qYuJoebSZnZdn'
-        var data = req.body;
-
-        var config = {
-        method: 'post',
-        url: 'https://api.dev-sheba.xyz/v1/ecom-payment/initiate',
-        headers: { 
-            'client-id': client_id, 
-            'client-secret': client_secret,
-            'Accept': 'application/json', 
-            'Content-Type': 'application/json'
-        },
-            data : data
-        };
-
-        axios(config)
-        .then(function (response) {
-            res.send(response.data);
-        })
-        .catch(function (error) {
-            res.send(error);
-        });
-
-    })
-})
-
-client.connect(error => {
     const luckyWinnerCollection = client.db("bandhon_ecommerce").collection("lucky_winner_data")
 
     app.get('/get-lucky-winner-data', (req, res) => {
@@ -156,9 +124,7 @@ client.connect(error => {
         })
         .catch(err => console.log(err))
     })
-})
 
-client.connect(error => {
     const campaignCollection = client.db("bandhon_ecommerce").collection("campaign")
 
     app.get('/get-campaign-data', (req, res) => {
@@ -181,9 +147,7 @@ client.connect(error => {
         .then(result =>  res.send(result))
         .catch(err => res.send(err))
     })
-})
 
-client.connect(error => {
     const adminDataCollection = client.db("bandhon_ecommerce").collection("admin_mail")
 
     app.get('/get-admin-mail', (req, res) => {
@@ -193,6 +157,164 @@ client.connect(error => {
             console.log(err)
         })
     })
+
+    const campaignDataCollection = client.db("bandhon_ecommerce").collection("campaign_data")
+
+    // calling sManager payment gateway api
+    app.post('/call-payment-gateway', (req, res) => {
+        const client_id = '721182060'
+        const client_secret = 'p3PO1ZmZWMM4msFBEubuwD2lTSQvXdu8zDIh2jJLfqjz9zTXJotl86JO6wHRck5zO6edx1KdML2XQkfu57r1a2s84jPIfdJYglebLnPWDFacDt9e4K1tHozd'
+        var data = req.body;
+
+        var config = {
+        method: 'post',
+        url: 'https://api.sheba.xyz/v1/ecom-payment/initiate',
+        headers: { 
+            'client-id': client_id,
+            'client-secret': client_secret,
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json'
+        },
+            data : data
+        };
+
+        axios(config)
+        .then(function (response) {
+            res.send(response.data);
+        })
+        .catch(function (error) {
+            res.send(error);
+        });
+    })
+
+    // getting payment details from sManager
+    app.post('/add-payment-details', (req, res) => {
+        var paymentData = req.body;
+
+        const client_id = '664520543'
+        const client_secret = 'E2BC3ADUArXkyjpJJe6nWcBhDbLwBbY5EVQ1yfdwAXPafUnAZcysJm0zQ98Y4TQriiKcZQvQgRsZxlAaDeLZCK5msWzwLRWB4aPHNN1ZTH4qYuJoebSZnZdn'
+        var axios = require('axios');
+        var data = JSON.stringify({
+        "transaction_id": paymentData.transactionId
+        });
+
+        var config = {
+        method: 'get',
+        url: 'https://api.dev-sheba.xyz/v1/ecom-payment/details',
+        headers: { 
+            'Accept': 'application/json', 
+            'client-id': client_id, 
+            'client-secret': client_secret,
+            'Content-Type': 'application/json'
+        },
+            data : data
+        };
+
+        axios(config)
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    })
+
+    // getting campaign payment details from sManager
+    app.post('/add-campaign-payment-details', (req, res) => {
+        var campaignPaymentData = req.body;
+
+        const client_id = '721182060'
+        const client_secret = 'p3PO1ZmZWMM4msFBEubuwD2lTSQvXdu8zDIh2jJLfqjz9zTXJotl86JO6wHRck5zO6edx1KdML2XQkfu57r1a2s84jPIfdJYglebLnPWDFacDt9e4K1tHozd'
+        var axios = require('axios');
+        var data = JSON.stringify({
+        "transaction_id": campaignPaymentData.transactionId
+        });
+
+        var config = {
+        method: 'get',
+        url: 'https://api.sheba.xyz/v1/ecom-payment/details',
+        headers: { 
+            'Accept': 'application/json', 
+            'client-id': client_id, 
+            'client-secret': client_secret,
+            'Content-Type': 'application/json'
+        },
+            data : data
+        };
+
+        axios(config)
+        .then(function (response) {
+            campaignDataCollection.find({})
+            .toArray((err, docs) => {
+                var CPCategoryData = docs.find(category => category.name === campaignPaymentData.productDetails.productCategory)
+                var addNewData = {...campaignPaymentData}
+                if(CPCategoryData.data){
+                    addNewData.code = parseInt(CPCategoryData.data[CPCategoryData.data.length - 1].code) + 1
+                    campaignDataCollection.updateOne(
+                        { _id: ObjectId(CPCategoryData._id) },
+                        {
+                        $set: {data: [...CPCategoryData.data, addNewData]},
+                        }
+                    )
+                    .then(result => {
+                        userDataCollection.find({})
+                        .toArray((err, docs) => {
+                            const userData = docs.find(user => user._id == campaignPaymentData.userId)
+                            if(userData.campaignProducts) {
+                                userDataCollection.updateOne(
+                                    { _id: ObjectId(userData._id) },
+                                    {
+                                    $set: {campaignProducts: [...userData.campaignProducts, addNewData]},
+                                    }
+                                )
+                                .then(result =>  res.send(result))
+                                .catch(err => res.send(err));
+                            }
+                            else {
+                                userDataCollection.updateOne(
+                                    { _id: ObjectId(userData._id) },
+                                    {
+                                    $set: {campaignProducts: [addNewData]},
+                                    }
+                                )
+                                .then(result =>  res.send(result))
+                                .catch(err => res.send(err));
+                            }
+                        })
+                    })
+                    .catch(err => res.send(err))
+                }
+                else{
+                    addNewData.code = 2021
+                    campaignDataCollection.updateOne(
+                        { _id: ObjectId(CPCategoryData._id) },
+                        {
+                        $set: {data: [addNewData]},
+                        }
+                    )
+                    .then(result => {
+                        userDataCollection.find({})
+                        .toArray((err, docs) => {
+                            const userData = docs.find(user => user._id == campaignPaymentData.userId)
+                            userDataCollection.updateOne(
+                                { _id: ObjectId(userData._id) },
+                                {
+                                $set: {campaignProducts: [addNewData]},
+                                }
+                            )
+                            .then(result =>  res.send(result))
+                            .catch(err => res.send(err));
+                        })
+                    })
+                    .catch(err => res.send(err))
+                }
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    })
+
 })
 
 app.listen(process.env.PORT || 5000)
